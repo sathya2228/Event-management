5329f893ad176b45e85cbc6aef167178083c5ba7
RNSsanjay <sanjay.n.aiml.2022@snsct.org>
2025-07-22 18:08:14 +0530

    final push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import './App.css';
 // Import your components with correct paths
 import AdminSignup from './pages/admin/adminsignup.jsx';
 import AdminLogin from './pages/admin/adminlogin';
-import EventHive from './pages/admin/adminevents.jsx';
 import CreateEvent from './pages/admin/createevent.jsx';
+import AdminEvents from './pages/admin/events.jsx';
 
 function App() {
   return (
@@ -14,8 +14,9 @@ function App() {
         {/* Admin Routes */}
         <Route path="/signup" element={<AdminSignup />} />
         <Route path="/" element={<AdminLogin />} />
-        <Route path="/EventHive" element={<EventHive />} />
+    
         <Route path="/createevent" element={<CreateEvent />} />
+        <Route path="/events" element={<AdminEvents />} />
 
         {/* Add more routes as needed */}
         {/* User Routes can be added similarly */}
